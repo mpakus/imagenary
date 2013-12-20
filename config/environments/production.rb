cf32608e0c7fd4e6dd0493b5b405816c46c82ea8
MpaK <mrak69@gmail.com>
2013-12-21 00:03:50 +0700

    Shows correct 404 error page on Photos#show

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-wImagenary::Application.configure do
+Imagenary::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
