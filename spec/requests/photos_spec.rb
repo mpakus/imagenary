4bcfc96df5f850de86c025217931d6b1ddd85f7b
MpaK <mrak69@gmail.com>
2013-12-22 14:11:15 +0700

    Changed to_not eq('') to be_present

--- spec/requests/photos_spec.rb
+++ spec/requests/photos_spec.rb
@@ -15,9 +15,9 @@ describe "Requests to /photos.json" do
     expect(response.body.blank?).to_not be_true
     json = JSON.parse(response.body)
     expect(json['status']['code']).to eq(200)
-    expect(json['photo']['id']).to_not eq('')
-    expect(json['photo']['image']['thumb']).to_not eq('')
-    expect(json['photo']['image']['full']).to_not eq('')
+    expect(json['photo']['id']).to be_present
+    expect(json['photo']['image']['thumb']).to be_present
+    expect(json['photo']['image']['full']).to be_present
   end
 
   it "with wrong token it's finish with 404 error" do
